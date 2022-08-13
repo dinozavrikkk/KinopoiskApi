ec19f9c2a6b2a7b8bde5cf34eb2a54d0ca8e82a8
Ayaz <sabitov.aiaz@yandex.ru>
2022-08-13 15:00:02 +0300

    added Service

--- Kinopoisk/TopFilmsScreen/DataModel/Service.swift
+++ Kinopoisk/TopFilmsScreen/DataModel/Service.swift
@@ -1,40 +1,39 @@
 
-//import Foundation
-//
-//final class Service {
-//
-//    private let queue = DispatchQueue(label: "com.TopFilmsDataProvider.URLSession", qos: .userInteractive, attributes: .concurrent)
-//    private var currentPage: Int = 1
-//
-//    static let shared: Service = Service()
-//
-//    private init() { }
-//
-//    func goingOnline() {
-//        queue.async { [weak self] in
-//            guard let self = self else { return }
-//            guard let url = URL(string: "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?page=\(self.currentPage)") else { return }
-//
-//            self.currentPage += 1
-//            var request = URLRequest(url: url)
-//            request.setValue("c3b53a94-80d5-41b3-a667-2424d1bfd9ad", forHTTPHeaderField: "X-API-KEY")
-//            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-//                if let data = data, let topFilms = try? JSONDecoder().decode(Kino.self, from: data) {
-//                    DispatchQueue.main.async {
-//                        completion(.success(topFilms))
-//                    }
-//                } else {
-//                    self.currentPage -= 1
-//                    guard let error = error else { return }
-//                    DispatchQueue.main.async {
-//                        completion(.failure(error))
-//                    }
-//                }
-//            }
-//            task.resume()
-//        }
-//
-//    }
-//
-//}
+import Foundation
+
+final class Service {
+
+    private let queue = DispatchQueue(label: "com.TopFilmsDataProvider.URLSession", qos: .userInteractive, attributes: .concurrent)
+    private var currentPage: Int = 1
+
+    static let shared: Service = Service()
+
+    private init() { }
+
+    func goingOnline(completion: @escaping (Result<Kino, Error>) -> Void) {
+        queue.async { [weak self] in
+            guard let self = self else { return }
+            guard let url = URL(string: "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?page=\(self.currentPage)") else { return }
+            
+            self.currentPage += 1
+            var request = URLRequest(url: url)
+            request.setValue("c3b53a94-80d5-41b3-a667-2424d1bfd9ad", forHTTPHeaderField: "X-API-KEY")
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let data = data, let topFilms = try? JSONDecoder().decode(Kino.self, from: data) {
+                    DispatchQueue.main.async {
+                        completion(.success(topFilms))
+                    }
+                } else {
+                    self.currentPage -= 1
+                    guard let error = error else { return }
+                    DispatchQueue.main.async {
+                        completion(.failure(error))
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
+}
--- Kinopoisk/TopFilmsScreen/DataModel/TopFilmsDataProvider.swift
+++ Kinopoisk/TopFilmsScreen/DataModel/TopFilmsDataProvider.swift
@@ -4,8 +4,7 @@ import UIKit
 
 final class TopFilmsDataProvider {
     
-    private let queue = DispatchQueue(label: "com.TopFilmsDataProvider.URLSession", qos: .userInteractive, attributes: .concurrent)
-    private var currentPage: Int = 1
+
     
 }
 
@@ -13,29 +12,13 @@ final class TopFilmsDataProvider {
 extension TopFilmsDataProvider: TopFilmsDataProviding {
     
     func gettingData(completion: @escaping (Result<Kino, Error>) -> Void) {
-        queue.async { [weak self] in
-            guard let self = self else { return }
-            guard let url = URL(string: "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?page=\(self.currentPage)") else { return }
-            
-            self.currentPage += 1
-            var request = URLRequest(url: url)
-            request.setValue("c3b53a94-80d5-41b3-a667-2424d1bfd9ad", forHTTPHeaderField: "X-API-KEY")
-            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-                if let data = data, let topFilms = try? JSONDecoder().decode(Kino.self, from: data) {
-                    DispatchQueue.main.async {
-//                        TopFilmsAssistent.shared.fillingArrayWithData(data: topFilms)
-                        completion(.success(topFilms))
-                    }
-                } else {
-                    self.currentPage -= 1
-                    guard let error = error else { return }
-                    DispatchQueue.main.async {
-                        completion(.failure(error))
-                    }
-                }
+        Service.shared.goingOnline { result in
+            switch result {
+            case .success(let model):
+                completion(.success(model))
+            case .failure(let error):
+                completion(.failure(error))
             }
-            task.resume()
         }
     }
     
